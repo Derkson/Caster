5226330dc1299648249b087accec437ec5ee605d
Derkson <derksonjoshua@gmail.com>
2021-11-23 20:10:05 -0700

    Filling out the factorio json

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from json import load
 
-if __name__ == 'main':
+if __name__ == '__main__':
     #Here goes nothing
     assert(len(sys.argv) == 2)
     file = open(sys.argv[1],'r')
 
-    sysdict = load(file)
-    print(sysdict)
+    contextdict = load(file)
+    print(contextdict)
+
+    
